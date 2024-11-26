6f4886cc83fed22a0173db76da4c7d2562719850
paoladjobissie <paoladjobissie@gmail.com>
2024-11-25 21:35:13 -0500

    Adding Education and certifications for my portfolio

--- src/data/constants.js
+++ src/data/constants.js
@@ -57,9 +57,9 @@ export const skills = [
     title: "Virtualization",
     skills: [
       {
-        name: "Java",
+        name: "Red Hat",
         image:
-          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+          "https://linprofs.com/wp-content/uploads/2024/08/redhat-enterprise-virtualization.png",
       },
       {
         name: "Microsoft Hyper-V",
@@ -79,7 +79,7 @@ export const skills = [
     ],
   },
   {
-    title: "Security",
+    title: "Azure Tools & Others",
     skills: [
       {
         name: "IAM",
@@ -101,10 +101,70 @@ export const skills = [
         image:
           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQa3fVP_LarPpUUBYGNxiDMl2vPRi5Jr1b-PA&s",
       },
+      {
+        name: "Azure Backup",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYxL9dHYRKZaJSj26ZV5wcP9_D7kDshXB6VA&s",
+      },
+      {
+        name: "Azure Disaster Recovery",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYxL9dHYRKZaJSj26ZV5wcP9_D7kDshXB6VA&s",
+      },
+      {
+        name: "Azure Advisor",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT27MD-T5nU29HbWRsHsK4lL-qSu9kgZfJnJw&s",
+      },
+      {
+        name: "Azure Policy",
+        image:
+          "https://i0.wp.com/anfcommunity.com/wp-content/uploads/2021/08/azurepolicy.png?resize=2000%2C1200&ssl=1",
+      },
+      {
+        name: "Microsoft Entra ID",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMKEasYNJcvlR1LEtrN5G0pnfvgTBrwZms5g&s",
+      },
+      {
+        name: "Azure Monitor",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQefoNDQKecyMqk6s6US0j3BvbgGemNJTrlAg&s",
+      },
+      {
+        name: "Azure Bastion",
+        image:
+          "https://azure.microsoft.com/svghandler/azure-bastion/?width=600&height=315",
+      },
+      {
+        name: "Azure Key Vault",
+        image:
+          "https://images.ctfassets.net/xxmwcynv5jdx/3ZK71BKxLiN9kEj5kGdgrz/9df4dc26f3284ed1ba846d7ff0c55759/azure-key-vault.jpg?w=1600&h=650&q=50&fit=fill&f=center",
+      },
+      {
+        name: "Microsoft Sentinel",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJ8akWIPLWCgVeKsfotEfL5-lwqwk_xiQNiQ&s",
+      },
+      {
+        name: "Kubernetes",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrr1joP9_yHRKTB8y9zgSKA3rFfoQ8rsdJLw&s",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name: "VPN",
+        image:
+          "https://www.ipxo.com/app/uploads/2021/12/What-is-VPN-image-820x460.jpg",
+      },
     ],
   },
   {
-    title: "Others",
+    title: "Cloud Computing Tools & Others",
     skills: [
       {
         name: "Git",
@@ -116,9 +176,9 @@ export const skills = [
           "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
       },
       {
-        name: "Docker",
+        name: "Java",
         image:
-          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
       },
       {
         name: "Netlify",
@@ -149,19 +209,29 @@ export const skills = [
           "https://marvel-b1-cdn.bc0a.com/f00000000236551/dt-cdn.net/wp-content/uploads/2021/07/13429_ILL_DevOpsLoop.png",
       },
       {
-        name: "Microsoft Entra ID",
+        name: "Azure SQL Database",
         image:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMKEasYNJcvlR1LEtrN5G0pnfvgTBrwZms5g&s",
+          "https://devblogs.microsoft.com/wp-content/uploads/sites/43/2019/03/Azure-SQL-Database.png",
       },
       {
-        name: "Azure Disaster Recovery",
+        name: "Azure Storage",
         image:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYxL9dHYRKZaJSj26ZV5wcP9_D7kDshXB6VA&s",
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbsWf1pzikNuPKVUJA1b4JxHssK-0pxYtxnw&s",
       },
       {
-        name: "Azure Backup",
+        name: "Azure API",
         image:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYxL9dHYRKZaJSj26ZV5wcP9_D7kDshXB6VA&s",
+          "https://chriskirby.net/content/images/2023/01/azure-storage-header.png",
+      },
+      {
+        name: "Terraform",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZcbnaLp6sh-O1VLp1aFiuIWB4TU-MTR0LYA&s",
+      },
+      {
+        name: "CloudWatch",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpt5BrKhQyWtgTSPKj5emFdnr_mgJdDMqC3A&s",
       },
     ],
   },
@@ -170,13 +240,75 @@ export const skills = [
 export const experiences = [
   {
     id: 0,
-    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
-    role: "DevOps & Fullstack Engineering Intern",
-    company: "Flipr Innovations Pvt. Ltd.",
-    date: "Aug 2023 - July 2023",
-    desc: "Working on Flipr Platforms, managing DevOps, and streamlining the process with automation.",
-    skills: ["Docker", "Terraform", "AWS", "EC2", "Portainer", "Nginx"],
+    img: "https://ausbestworkplaces2021.economist.com/images/logos/UKG_Logo.png",
+    role: "Mid-Level Azure Cloud Engineer",
+    company: "UKG Ultimate Software Group.",
+    date: "03/2023 to Present",
+    desc: "Working on Designing and implementing a multi-region infrastructure with Traffic Manager and Load Balancers, achieving 99.99% availability and minimal latency across regions,Automated infrastructure provisioning with Terraform and ARM templates, reducing deployment times by 60% and minimizing errors, Led security improvements with Azure AD and NSGs to enforce role-based access control, reducing unauthorized access incidents by 40%,Enhanced incident management through Azure Monitor and Log Analytics, integrating alerting workflows via Logic Apps to achieve under 5-minute critical resolution times,Collaborated cross-functionally to establish compliance frameworks for Azure resources, ensuring adherence to corporate security and compliance standards.",
+
+    skills: [
+      "Docker",
+      "Terraform",
+      "AWS",
+      "EC2",
+      "Portainer",
+      "Nginx",
+      "JavaScript",
+      "TypeScript",
+      "Node Js",
+      " Next Js",
+    ],
+    doc: "",
   },
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/v2/C560BAQHwKThSinRiRw/company-logo_200_200/company-logo_200_200/0/1657267480326/mcdns_logo?e=2147483647&v=beta&t=8YH1YegdjrTlgJIavfMtAE83OrKWx6qouoPe7eht3dY",
+    role: "Azure Cloud Engineer",
+    company: "MCdns LLC",
+    date: "02/2022 – 03/2023",
+    desc: "Worked on the Configuration and optimization Virtual Machines, Azure Storage, and networking components, improving application performance by 25%,Developed PowerShell scripts for resource automation, reducing manual configuration time by 40%,Implemented cost optimization strategies, resulting in a 30% reduction in spend through reserved instance planning and performance tuning,Established secure VNet architectures and configured Application Gateways to control traffic flow and enhance security,Integrated Azure Sentinel with custom alert rules for proactive monitoring, reducing response time to security incidents by 50%.",
+    skills: [
+      "ReactJS",
+      "Redux",
+      "NodeJs",
+      "Material UI",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    doc: "",
+  },
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuw6wwD9bnQdKKTdD77Ci6lmncfiqv1GCiRA&s",
+    role: "Junior Azure Cloud Engineer",
+    company: "SafeGuard International – Atlanta, GA",
+    date: "10/2020 – 02/2022",
+    desc: "Provided assistance in managing a hybrid cloud environment, supporting Azure and on-premises resources to ensure seamless connectivity,Configured Azure Backup solutions, achieving a 99.9% data recovery success rate for critical resources,Set up virtual networks (VNets), subnets, and NSGs to improve network isolation and secure Azure-based workloads,Supported Tier-2 troubleshooting for Azure resources, resolving network and storage issues that increased system uptime by 15%,Implemented Azure AD for user identity and access management, centralizing authentication across cloud resources.",
+    skills: [
+      "ReactJS",
+      "Redux",
+      "NodeJs",
+      "Material UI",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Docker",
+      "AWS",
+      "MongoDB",
+    ],
+  },
+  {
+    id: 2,
+    img: "https://mms.businesswire.com/media/20220204005229/en/1077269/23/Labcorp_Logo_Horizontal_Color_RGB.jpg",
+    role: "Azure Cloud Operations Associate",
+    company: "LabCorp",
+    date: "03/2019 – 10/2020",
+    desc: "Deployed and maintained scalable microservices in Azure, supporting daily operations for over 400 active users,Reduced costs by 20% through rightsizing VMs and implementing auto-scaling policies based on traffic patterns,Developed and maintained CI/CD pipelines using GitHub Actions, accelerating release cycles by 50%,Built custom dashboards in Azure Monitor to track system health, providing real-time insights that improved response time to issues,Enforced best practices in Azure security, configuring IAM roles, and data encryption to achieve SOC 2 compliance.",
+    doc: "",
+  },
+ 
+
   // Other experiences...
 ];
 
@@ -185,10 +317,63 @@ export const education = [
     id: 0,
     img: "https://mms.businesswire.com/media/20241023977069/en/813216/5/DVU_LOGO_BusinessWire.jpg",
     school: "DeVry University in Lisle, Illinois",
-    date: "January 2023 - March 2025",
+    date: "Expected in April 2025",
     desc: "Currently pursuing a Bachelor's degree in Information Technology and Networking.",
     degree: "Bachelor of Information Technology and Networking",
   },
+  {
+    id: 0,
+    img: "https://mms.businesswire.com/media/20241023977069/en/813216/5/DVU_LOGO_BusinessWire.jpg",
+    school: "DeVry University in Lisle, Illinois",
+    date: "Jan 2021 - April 2023",
+    desc: "I have complited my Associate degree in Applied science of Information Technology and Networking.",
+    degree: "Associate of Information Technology and Networking",
+  },
+  {
+    id: 0,
+    img: "https://media.assettype.com/analyticsinsight%2F2024-07%2Feaab4424-0962-4f9a-aded-58317836c1d2%2FGreat_Learning12_1024x682.png",
+    school: "University of Texas at Austin/Great Learning",
+    date: "Jan 2024 - July 2024",
+    desc: "I have completed the Post Graduate Program in Cloud Computing with Great Learning through University of Texas at Austin, where I have gained multiples knowledges and Certifications on AWS cloud computing.",
+    degree: "Post Graduate Program in AWS Cloud Computing",
+  },
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSX1z5XvAwS8G_ojSep2CpJdkYJ7MhpUvJ_8g&s",
+    school: "MICROSOFT",
+    date: "2022",
+    grade: "",
+    desc: "",
+    degree: "Microsoft Certified: Azure Administrator (AZ-900)",
+  },
+  {
+    id: 1,
+    img: "https://learn.microsoft.com/en-us/media/learn/certification/badges/microsoft-certified-associate-badge.svg",
+    school: "MICROSOFT",
+    date: "",
+    grade: "",
+    desc: "",
+    degree: "Microsoft Certified: Azure Administrator (AZ-104)",
+  },
+  {
+    id: 2,
+    img: "https://learn.microsoft.com/en-us/media/learn/certification/badges/microsoft-certified-associate-badge.svg",
+    school: "MICROSOFT",
+    date: "",
+    grade: "",
+    desc: "",
+    degree: "Azure Security Engineer (AZ-500)",
+  },
+  {
+    id: 2,
+    img: "https://img-c.udemycdn.com/open-badges/v2/badge-class/2091334622/image3162068384381728142.png",
+    school: "MICROSOFT",
+    date: "",
+    grade: "",
+    desc: "",
+    degree: "Cyber Security Expert (SC-100)",
+  },
+
   // Other education...
 ];
 
