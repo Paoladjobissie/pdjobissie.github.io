481f1a741704533a1ad3551fea3dac6505b7ec81
paoladjobissie <paoladjobissie@gmail.com>
2024-11-30 17:13:01 -0500

    added changes on contact

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -151,7 +151,7 @@ const Contact = () => {
           <ContactInput placeholder="Your Email" name="from_email" />
           <ContactInput placeholder="Your Name" name="from_name" />
           <ContactInput placeholder="Subject" name="subject" />
-          <ContactInputMessage placeholder="Message" rows="4" name="message" />
+          <ContactInputMessage placeholder="Message" rows="4" name="paola" />
           <ContactButton type="submit" value="Send" />
         </ContactForm>
         <Snackbar
